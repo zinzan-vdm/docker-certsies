4223460ba4a11ec9c46d3911d849187443236c87
Zinzan <inq2111@gmail.com>
2022-05-09 01:44:33 +0200

    Added the ability to specify custom domains.txt/lexicon.yml paths to be configured on start

--- app/configure.sh
+++ app/configure.sh
@@ -17,9 +17,19 @@ mkdir -p "$CERTSIES_DIR/wellknown"
 mkdir -p "$CERTSIES_DIR/certs"
 mkdir -p "$CERTSIES_DIR/accounts"
 
-if [[ -d '/conf' ]]; then
+if [[ -d /conf ]]; then
   echo "Located directory /conf on the filesystem. Any files inside this folder will now be copied to the $CERTSIES_DIR."
   cp -r /conf/* "$CERTSIES_DIR"
+fi
+
+if [[ ! -z $DOMAINS_TXT && -f $DOMAINS_TXT ]]; then
+  echo "Config specifies DOMAINS_TXT ($DOMAINS_TXT), copying file to $CERTSIES_DIR."
+  cp "$DOMAINS_TXT" "$CERTSIES_DIR/domains.txt"
+fi
+
+if [[ ! -z $LEXICON_YML && -f $LEXICON_YML ]]; then
+  echo "Config specifies LEXICON_YML ($LEXICON_YML), copying file to $CERTSIES_DIR."
+  cp "$LEXICON_YML" "$CERTSIES_DIR/lexicon.yml"
 fi
 
 echo "Generating $CERTSIES_DIR/config."
